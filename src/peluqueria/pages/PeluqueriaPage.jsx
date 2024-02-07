5c1da46938e452bef8ef22aa67542e0d01e04d35
sebastian461 <sebastiano-461@hotmail.com>
2024-02-07 17:40:03 -0500

    react-big-calendar
    Added react-big-calendar to display events in a calendar

--- src/peluqueria/pages/PeluqueriaPage.jsx
+++ src/peluqueria/pages/PeluqueriaPage.jsx
@@ -1,9 +1,42 @@
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours } from "date-fns";
+
 import { Navbar } from "../components";
+import { getMessagesEs, localizer } from "../../helpers";
+
+const events = [
+  {
+    title: "Corte normal",
+    amount: 2.5,
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    user: {
+      id: 1,
+      name: "Sebastián",
+    },
+  },
+];
 
 export const PeluqueriaPage = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log({ event, start, end, isSelected });
+  };
+
   return (
     <>
       <Navbar />
+
+      <Calendar
+        culture="es"
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+        messages={getMessagesEs()}
+        eventPropGetter={eventStyleGetter}
+      />
     </>
   );
 };
